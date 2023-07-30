385cd67a188d8ee786cf92240432486c4f5f040c
kuailiaojie <128234728+kuailiaojie@users.noreply.github.com>
2023-07-30 19:25:17 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,13 +8,13 @@ export const SITE_ENV =
 
 // Copyright links
 // Do not modify please
-export const GITHUB_OWNER = 'FreeNowOrg'
+export const GITHUB_OWNER = 'kuailiaojie'
 export const GITHUB_REPO = 'PixivNow'
 export const GITHUB_URL = `https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}`
 
 // Site name
 export const PROJECT_NAME = 'PixivNow'
-export const PROJECT_TAGLINE = 'Pixiv Service Proxy (pixiv.js.org)'
+export const PROJECT_TAGLINE = 'Pixiv Service Proxy '
 
 // Image proxy cache seconds
 export const IMAGE_CACHE_SECONDS = 12 * 60 * 60 * 1000
